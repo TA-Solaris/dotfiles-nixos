8c9cc88740d5cb1c5e971b3b6dba60da6c317b8e
Edward Potter <pottered2@gmail.com>
2025-08-30 23:10:36 +0100

    Gnome extensions install

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -25,6 +25,22 @@
     environment.systemPackages = [ pkgs.gnomeExtensions.appindicator  ];
     services.udev.packages = [ pkgs.gnome-settings-daemon ];
     
+    # Other extensions
+    environment.systemPackages = [
+      pkgs.gnomeExtensions.blur-my-shell
+      pkgs.gnomeExtensions.caffeine
+      pkgs.gnomeExtensions.clipboard-indicator
+      pkgs.gnomeExtensions.dash-to-dock
+      pkgs.gnomeExtensions.desktop-icons-ng
+      pkgs.gnomeExtensions.just-perfection
+      pkgs.gnomeExtensions.panel-scroll
+      pkgs.gnomeExtensions.rounded-corners
+      pkgs.gnomeExtensions.rounded-window-corners-reborn
+      pkgs.gnomeExtensions.vitals
+      pkgs.gnomeExtensions.removable-drive-menu
+      pkgs.gnomeExtensions.screenshot-window-sizer
+    ];
+    
     # Gnome games
     services.gnome.games.enable = true;
   };
