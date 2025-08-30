7a517f61e3c435dc30dc714d620cfbb80756c040
Edward Potter <pottered2@gmail.com>
2025-08-30 16:42:21 +0100

    Neovim custom config

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -8,6 +8,39 @@
     home.packages = [
       inputs.nvf.packages.${pkgs.system}.default
     ];
+    
+    programs.nvf = {
+      enable = true;
+      
+      settings = {
+        colorscheme = "tokyonight";
+      };
+    
+      plugins = {
+        telescope.enable = true;
+        treesitter.enable = true;
+        lsp.enable = true;
+        tokyonight.enable = true;
+      };
+      
+      languages = {
+        enableLSP = true;
+        enableTreesitter = true;
+      
+        rust.enable = true;
+        nix.enable = true;
+        sql.enable = true;
+        clang.enable = true;
+        ts.enable = true;
+        python.enable = true;
+        markdown.enable = true;
+        html.enable = true;
+        dart.enable = true;
+        go.enable = true;
+        lua.enable = true;
+        php.enable = true;
+      };
+    };
 
     home.sessionVariables.EDITOR = "nvim";
   };
