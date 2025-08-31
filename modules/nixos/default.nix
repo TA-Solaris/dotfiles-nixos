d9c9fa2b3f81924c3066a148e6f1871476c7c3dc
Edward Potter <pottered2@gmail.com>
2025-08-31 20:06:45 +0100

    Fix fr?

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -6,7 +6,6 @@
     ./pipewire.nix
     ./network-manager.nix
     ./fonts.nix
-    ./dconf-config.nix
   ];
   
   users.ed.enable = lib.mkDefault true;
@@ -14,6 +13,5 @@
   pipewire.enable = lib.mkDefault true;
   network-manager.enable = lib.mkDefault true;
   fonts.enable = lib.mkDefault true;
-  dconf-config.enable = lib.mkDefault true;
 
 }
