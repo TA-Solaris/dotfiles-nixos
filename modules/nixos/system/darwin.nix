4fef7a68fc42980b5d83a865bc073bf3e95b36d7
Edward Potter <ep@incyan.com>
2025-10-08 12:35:05 +0100

    Add wireguard to Darwin

--- modules/nixos/system/darwin.nix
+++ modules/nixos/system/darwin.nix
@@ -7,11 +7,13 @@
   imports = [
     ./fonts-darwin.nix
     ./git.nix
+    ./wireguard.nix
   ];
 
   config = lib.mkIf config.nixos-system.enable {
     fonts.enable = lib.mkDefault true;
     git.enable = lib.mkDefault true;
+    wireguard.enable = lib.mkDefault true;
   };
 
 }
