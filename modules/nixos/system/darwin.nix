0dce517e4fb54c13ec6a3da443bfbd2328d68b03
Edward Potter <ep@incyan.com>
2025-10-19 23:04:32 +0100

    Some mac settings

--- modules/nixos/system/darwin.nix
+++ modules/nixos/system/darwin.nix
@@ -8,12 +8,14 @@
     ./fonts-darwin.nix
     ./git.nix
     ./wireguard.nix
+    ./macos.nix
   ];
 
   config = lib.mkIf config.nixos-system.enable {
     fonts.enable = lib.mkDefault true;
     git.enable = lib.mkDefault true;
     wireguard.enable = lib.mkDefault true;
+    macos.enable = lib.mkDefault true;
   };
 
 }
---
+++ modules/nixos/system/macos.nix
@@ -0,0 +1,48 @@
+{ pkgs, lib, config, ... }: {
+
+  options = {
+    macos.enable = lib.mkEnableOption "enable macos config";
+  };
+
+  config = lib.mkIf config.macos.enable {
+    # Asthetic
+    system.defaults.controlcenter.BatteryShowPercentage = true;
+    system.defaults.NSGlobalDomain.NSStatusItemSpacing = 0;
+    system.defaults.NSGlobalDomain.NSStatusItemSelectionPadding = 0;
+
+    # Disable hot corners
+    system.defaults.dock.wvous-bl-corner = 1;
+    system.defaults.dock.wvous-br-corner = 1;
+    system.defaults.dock.wvous-tl-corner = 1;
+    system.defaults.dock.wvous-tr-corner = 1;
+    
+    # Dock
+    system.defaults.dock.autohide = true;
+    system.defaults.dock.show-recents = false;
+
+    # Clock
+    system.defaults.menuExtraClock.ShowSeconds = true;
+    system.defaults.menuExtraClock.ShowDayOfWeek = true;
+    system.defaults.menuExtraClock.Show24Hour = true;
+
+    # Finder
+    system.defaults.finder.ShowPathbar = true;
+    system.defaults.finder.AppleShowAllExtensions = true;
+    system.defaults.finder.AppleShowAllFiles = true;
+    system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
+    system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+
+    # System
+    system.defaults.SoftwareUpdate.AutomaticallyInstallMacOSUpdates = false;
+    system.defaults.NSGlobalDomain.NSDisableAutomaticTermination = true;
+    system.defaults.NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+    system.defaults.NSGlobalDomain.AppleTemperatureUnit = "Celsius";
+    system.defaults.NSGlobalDomain.AppleMeasurementUnits = "Centimeters";
+
+    # Interaction
+    system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false; # Not "Natural"
+    system.defaults.NSGlobalDomain.KeyRepeat = 6;
+    system.defaults.NSGlobalDomain.InitialKeyRepeat = 3;
+  };
+
+}
