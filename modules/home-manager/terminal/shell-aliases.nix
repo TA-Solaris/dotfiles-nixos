bb6b95e6c840f6c7cb0c8ce81ee0ce7b154cf829
Edward Potter <pottered2@gmail.com>
2025-10-20 14:43:38 +0100

    Nix clean command

--- modules/home-manager/terminal/shell-aliases.nix
+++ modules/home-manager/terminal/shell-aliases.nix
@@ -24,6 +24,7 @@
       c = "clear";
       h = "cd ~";
       hc = "cd ~; clear;";
+      nix-clean = "sudo nix-collect-garbage -d && nix-collect-garbage -d && home-manager expire-generations -d && nix store gc && sudo nix store optimize && sudo nix profile wipe-history && home-manager remove-generations old";
     };
   };
 }
