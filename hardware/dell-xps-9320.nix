714bdb23d1e5f0051c25f1718a179566ff9ad59f
Edward Potter <pottered2@gmail.com>
2025-10-21 22:40:44 +0100

    Some camera stuff

--- hardware/dell-xps-9320.nix
+++ hardware/dell-xps-9320.nix
@@ -4,6 +4,16 @@
   pkgs,
   ...
 }: {
+  # Packages
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModulePackages = [ pkgs.linuxPackages.v4l2loopback ];
+  environment.systemPackages = with pkgs; [
+    v4l-utils # https://discourse.nixos.org/t/v4l2loopback-cannot-find-module/26301/5
+    alsa-utils
+    libcamera
+    ivsc-firmware
+  ];
+
   # Fingerprint sensor
   services.fprintd.enable = true;
   
@@ -17,9 +27,6 @@
   
   # Microphone
   security.rtkit.enable = true;
-  environment.systemPackages = with pkgs; [
-    alsa-utils
-  ];
   # https://wiki.archlinux.org/title/Dell_XPS_13_Plus_(9320)#Audio
   # May need to run
   # alsactl init
