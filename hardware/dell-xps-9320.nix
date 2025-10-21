e232d9d738fa5909cbffbda251bd5592d46668d9
Edward Potter <pottered2@gmail.com>
2025-10-21 22:07:56 +0100

    Some audio stuff

--- hardware/dell-xps-9320.nix
+++ hardware/dell-xps-9320.nix
@@ -15,5 +15,14 @@
     Option "TearFree" "true"
   '';
   
+  # Microphone
+  security.rtkit.enable = true;
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+  ];
+  # https://wiki.archlinux.org/title/Dell_XPS_13_Plus_(9320)#Audio
+  # May need to run
+  # alsactl init
+  # alsactl store
   
 }
