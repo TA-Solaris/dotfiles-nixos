93fbb0a2e3ac7421a6d7e761cd33fb02ba946bc6
Edward Potter <pottered2@gmail.com>
2025-10-21 20:08:31 +0100

    Fix screen tearing

--- hardware/dell-xps-9320.nix
+++ hardware/dell-xps-9320.nix
@@ -6,4 +6,14 @@
 }: {
   # Fingerprint sensor
   services.fprintd.enable = true;
+  
+  # Fix screen tearing
+  # https://nixos.org/manual/nixos/stable/index.html#sec-x11--graphics-cards-intel
+  services.xserver.videoDrivers = [ "intel" ];
+  services.xserver.deviceSection = ''
+    Option "DRI" "2"
+    Option "TearFree" "true"
+  '';
+  
+  
 }
