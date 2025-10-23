8f09d51707878e64269a90631c42f2584ade23d2
Edward Potter <ep@incyan.com>
2025-10-23 14:25:27 +0100

    Fix option name

--- modules/nixos/dev/docker-darwin.nix
+++ modules/nixos/dev/docker-darwin.nix
@@ -1,13 +1,16 @@
-{ pkgs, lib, config, ... }: {
-
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   options = {
-    docker.enable = lib.mkEnableOption "enable docker";
+    docker-darwin.enable = lib.mkEnableOption "enable docker on darwin";
   };
 
-  config = lib.mkIf config.docker.enable {
+  config = lib.mkIf config.docker-darwin.enable {
     environment.systemPackages = [
       pkgs.docker
     ];
   };
-
 }
