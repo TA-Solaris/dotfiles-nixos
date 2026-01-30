749bb487e5f098aeee265ff5ea759da302b96e6a
Edward Potter <pottered2@gmail.com>
2026-01-30 11:16:01 +0000

    XPS Camera Drivers

--- hardware/dell-xps-9320.nix
+++ hardware/dell-xps-9320.nix
@@ -1,5 +1,3 @@
-# TODO Make camera work
-
 {
   config,
   lib,
@@ -8,27 +6,26 @@
 }: {
   # Fingerprint sensor
   services.fprintd.enable = true;
-  
+
   # Fix screen tearing
   # https://nixos.org/manual/nixos/stable/index.html#sec-x11--graphics-cards-intel
-  services.xserver.videoDrivers = [ "intel" ];
+  services.xserver.videoDrivers = ["intel"];
   services.xserver.deviceSection = ''
     Option "DRI" "2"
     Option "TearFree" "true"
   '';
-  
+
   # Enable hardware acceleration
   hardware.graphics.enable = true;
-  
+
   # Camera
   # Tracking Issue: Intel MIPI/IPU6 webcam-support
   # https://github.com/NixOS/nixpkgs/issues/225743#issuecomment-1849613797
   hardware.ipu6 = {
-    enable = false;
+    enable = true;
     platform = "ipu6ep";
   };
   environment.systemPackages = with pkgs; [
     libcamera
   ];
 }
-
