390033a1f591b1404a2c2501a77c0d026d7a1722
Edward Potter <pottered2@gmail.com>
2026-02-13 16:47:03 +0000

    Attempt to fix Nautilus

--- modules/nixos/system/gnome.nix
+++ modules/nixos/system/gnome.nix
@@ -74,5 +74,21 @@
     # Firmware upgrades
     services.fwupd.enable = true;
     hardware.enableRedistributableFirmware = true;
+
+    # Fix for gstreamer in Nautilus (https://nixos.wiki/wiki/Nautilus)
+    nixpkgs.overlays = [
+      (self: super: {
+        gnome = super.gnome.overrideScope (gself: gsuper: {
+          nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
+            buildInputs =
+              nsuper.buildInputs
+              ++ (with pkgs.gst_all_1; [
+                gst-plugins-good
+                gst-plugins-bad
+              ]);
+          });
+        });
+      })
+    ];
   };
 }
