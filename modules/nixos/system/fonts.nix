d293edb067fc5e00b87e41aa566221ca1eb8d3ce
Edward Potter <pottered2@gmail.com>
2026-08-28 11:37:42 +0100

    Fix wrong named files

--- modules/nixos/system/fonts.nix
+++ modules/nixos/system/fonts.nix
@@ -5,36 +5,12 @@
   ...
 }: {
   options = {
-    thumbnailers.enable = lib.mkEnableOption "enables thumbnailers";
+    fonts.enable = lib.mkEnableOption "enables fonts";
   };
 
-  config = lib.mkIf config.thumbnailers.enable {
-    # https://wiki.nixos.org/wiki/Thumbnails
-    environment.pathsToLink = [
-      "share/thumbnailers"
-    ];
-
+  config = lib.mkIf config.fonts.enable {
     environment.systemPackages = [
-      pkgs.ffmpeg-headless
-      pkgs.ffmpegthumbnailer
-      pkgs.gdk-pixbuf
-      # Allow gdk-pixbuf to thumbnail RAW photos by extracting the embedded jpeg
-      (pkgs.writeTextFile {
-        name = "raw-embedded-jpeg-thumbnailer";
-        destination = "/share/thumbnailers/raw-embedded-jpeg.thumbnailer";
-        text = ''
-          [Thumbnailer Entry]
-          TryExec=gdk-pixbuf-thumbnailer
-          Exec=gdk-pixbuf-thumbnailer -s %s %u %o
-          MimeType=image/x-canon-crw;image/x-canon-cr2;image/x-canon-cr3;image/x-adobe-dng;image/x-dng;
-        '';
-      })
-      pkgs.libheif.bin # provides heif-thumbnailer (the program that generates HEIF thumbnails)
-      pkgs.libheif.out # provides heif.thumbnailer (allows for the viewing of HEIF thumbnails)
-      pkgs.libavif
-      pkgs.libjxl
-      pkgs.webp-pixbuf-loader
-      pkgs.f3d
+      pkgs.nerd-fonts.jetbrains-mono
     ];
   };
 }
--- modules/nixos/system/thumbnailers.nix
+++ modules/nixos/system/thumbnailers.nix
@@ -1,13 +1,40 @@
-{ pkgs, lib, config, ... }: {
-  
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   options = {
-    fonts.enable = lib.mkEnableOption "enables fonts";
+    thumbnailers.enable = lib.mkEnableOption "enables thumbnailers";
   };
 
-  config = lib.mkIf config.fonts.enable {
+  config = lib.mkIf config.thumbnailers.enable {
+    # https://wiki.nixos.org/wiki/Thumbnails
+    environment.pathsToLink = [
+      "share/thumbnailers"
+    ];
+
     environment.systemPackages = [
-      pkgs.nerd-fonts.jetbrains-mono
+      pkgs.ffmpeg-headless
+      pkgs.ffmpegthumbnailer
+      pkgs.gdk-pixbuf
+      # Allow gdk-pixbuf to thumbnail RAW photos by extracting the embedded jpeg
+      (pkgs.writeTextFile {
+        name = "raw-embedded-jpeg-thumbnailer";
+        destination = "/share/thumbnailers/raw-embedded-jpeg.thumbnailer";
+        text = ''
+          [Thumbnailer Entry]
+          TryExec=gdk-pixbuf-thumbnailer
+          Exec=gdk-pixbuf-thumbnailer -s %s %u %o
+          MimeType=image/x-canon-crw;image/x-canon-cr2;image/x-canon-cr3;image/x-adobe-dng;image/x-dng;
+        '';
+      })
+      pkgs.libheif.bin # provides heif-thumbnailer (the program that generates HEIF thumbnails)
+      pkgs.libheif.out # provides heif.thumbnailer (allows for the viewing of HEIF thumbnails)
+      pkgs.libavif
+      pkgs.libjxl
+      pkgs.webp-pixbuf-loader
+      pkgs.f3d
     ];
   };
-
 }
