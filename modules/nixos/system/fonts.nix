09377f6fc47872f22a48ca9289bcd188fd8a5ce3
Edward Potter <pottered2@gmail.com>
2026-08-28 12:29:44 +0100

    Fonts

--- modules/nixos/system/fonts.nix
+++ modules/nixos/system/fonts.nix
@@ -9,8 +9,19 @@
   };
 
   config = lib.mkIf config.fonts.enable {
-    environment.systemPackages = [
-      pkgs.nerd-fonts.jetbrains-mono
+    environment.systemPackages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      font-manager
+    ];
+
+    fonts.packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      corefonts
+      helvetica-neue-lt-std
+      liberation_ttf
+      source-sans
+      vista-fonts
+      google-fonts
     ];
   };
 }
